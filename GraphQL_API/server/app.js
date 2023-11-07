9e4e4600087f3659da62ac048b83d1e42ed4af21
mnlazs <mnl.azs@gmail.com>
2023-11-07 14:48:22 -0600

    mongoDB

--- GraphQL_API/server/app.js
+++ GraphQL_API/server/app.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const {graphqlHTTP} = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// connect MongoDB Atlas database
+const mongoDBAtlasUri = "mongodb+srv://mnlazs:SZXoel449PtcspvJ@cluster0.xgnwb1j.mongodb.net/"
+mongoose.connect(mongoDBAtlasUri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
-
-
-app.use('/graphql',graphqlHTTP({
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
 }));
-app.listen(4000,()=>{
-  console.log('now listening for request on port 4000');
+
+app.listen(4000, () => {
+    console.log('now listening for request on port 4000');
 });
