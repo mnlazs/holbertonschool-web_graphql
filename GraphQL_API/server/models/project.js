baa044704485d776660ff4c758a6b8cfc688e875
mnlazs <mnl.azs@gmail.com>
2023-11-09 10:02:20 -0600

    updated

---
+++ GraphQL_API/server/models/project.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    title: String,
+    weight: Number,
+    description: String,
+});
+
+
+module.exports = mongoose.model ('Project', projectSchema );
--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -11,18 +11,6 @@ const {
 const projectModel = require('../node_modules/project');
 const Task = require('../node_modules/task');
 
-// Dummy data for the tasks
-// const tasks = [
-//     { id: '1', title: 'Create your first webpage', weight: 1, description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)', projectId: '1'},
-//     { id: '2', title: 'Structure your webpage', weight: 1, description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order', projectId: '1'},
-// ];
-
-// // Dummy data for the projects
-// const projects = [
-//     { id: '1', title: 'Advanced HTML', weight: 1, description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'},
-//     { id: '2', title: 'Bootstrap', weight: 1, description: 'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'},
-// ];
-
 // Definicion del tipo Task con dos parametros
 const TaskType = new GraphQLObjectType({
   name: 'Task',
